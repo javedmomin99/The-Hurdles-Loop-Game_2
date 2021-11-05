27d5a1321608f4616bf4a957700a2edb16955ee9
Mohammad Javed Momin <javedmomin99@gmail.com>
2021-11-05 18:07:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# Go to Website : https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
+# Clear the code if any code already present and paste the below written code and click on play button.
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+   
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+    jump()
